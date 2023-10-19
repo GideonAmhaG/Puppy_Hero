94d728cc6003a34604ce45d77461d6a96d0bdc5b
GideonAmhaG <gideonamha@gmail.com>
2023-10-19 20:04:27 +0300

    made space key the power key

--- Puppy_Hero/input.js
+++ Puppy_Hero/input.js
@@ -3,11 +3,12 @@ export class InputHandler {
         this.game = game;
         this.keys = [];
         window.addEventListener('keydown', e => {
+            console.log(e.key);
             if ((   e.key === 'ArrowDown' ||
                     e.key === 'ArrowUp' ||
                     e.key === 'ArrowLeft' ||
                     e.key === 'ArrowRight' ||
-                    e.key === 'Enter'
+                    e.key === ' '
                 ) && this.keys.indexOf(e.key) === -1) {
                 this.keys.push(e.key);
             } else if (e.key === 'd') this.game.debug = !this.game.debug;
@@ -17,7 +18,7 @@ export class InputHandler {
                     e.key === 'ArrowUp' ||
                     e.key === 'ArrowLeft' ||
                     e.key === 'ArrowRight' ||
-                    e.key === 'Enter') {
+                    e.key === ' ') {
                 this.keys.splice(this.keys.indexOf(e.key), 1);
             }
         });
